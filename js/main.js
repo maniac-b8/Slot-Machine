81b3d8fba4d1ab2d5457960e6d312f506e729917
maniacb8 <matthewdixon2021@gmail.com>
2024-03-30 12:43:28 -0700

    add cached elements

--- js/main.js
+++ js/main.js
@@ -14,7 +14,14 @@ let startingAmount = 100;
 let balance = startingAmount;
 
 /*----- cached elements  -----*/
-
+const spinButton = document.getElementById('spin');
+const wagerInput = document.getElementById('wager');
+const slot1Element = document.getElementById('slot1');
+const slot2Element = document.getElementById('slot2');
+const slot3Element = document.getElementById('slot3');
+const resultElement = document.getElementById('result');
+const balanceElement = document.getElementById('balance');
+const messageWindow = document.getElementById('message-window');
 
 /*----- event listeners -----*/
 
